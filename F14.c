88ce056a41b9130fe65124e77576e69d16449833
a <39839328+Andrefty@users.noreply.github.com>
2022-10-25 21:31:37 +0300

    Error handling and cleanup

--- F14.c
+++ F14.c
@@ -1,49 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+
 
 int main(int argc, char *argv[])
 {
     FILE *fin;
-    int index = 0, count = 0, i = 0, indin = -1, indsf = -1, indinv = -1, indsfv = -1;
+    int index = 0, count = 0, indin = -1, indsf = -1, indinv = -1, indsfv = -1;
+    char *pos;
     size_t nread;
     if (argc != 3)
     {
         fprintf(stderr, "Utilizare: %s sir fisier\n", argv[0]);
+        return 1;
+    }
+    fin = fopen(argv[2], "r");
+    if (fin == NULL)
+    {
+        perror(argv[2]);
         return 2;
     }
-    // char *fn = calloc(strlen(argv[2]) + 1, sizeof(char));
-    // if (fn == NULL)
-    // {
-    //     perror(argv[2]);
-    //     return 2;
-    // }
-    // char *str = calloc(strlen(argv[1]) + 1, sizeof(char));
-    // if (str == NULL)
-    // {
-    //     perror(argv[1]);
-    //     free(fn);
-    //     return 2;
-    // }
-    char *pos;
-
-    // char *txt=calloc(strlen(argv[1]) + 1, sizeof(char));
     char *txt1 = calloc(2 * (strlen(argv[1]) + 1), sizeof(char));
+    if (txt1 == NULL)
+    {
+        perror(txt1);
+        fclose(fin);
+        return 2;
+    }
     char *txt2 = calloc(strlen(argv[1]) + 1, sizeof(char));
+    if (txt2 == NULL)
+    {
+        perror(txt2);
+        free(txt1);
+        fclose(fin);
+        return 2;
+    }
     char *txt3 = calloc(strlen(argv[1]) + 1, sizeof(char));
-    // strcpy(str, argv[1]);
-    // strcpy(fn, argv[2]);
-    fin = fopen(argv[2], "r");
-    printf("%d\n", strlen(argv[1]));
-    // fgets(txt3, strlen(argv[1]) + 1, fin);
+    if (txt3 == NULL)
+    {
+        perror(txt3);
+        free(txt1);
+        free(txt2);
+        fclose(fin);
+        return 2;
+    }
     fread(txt3, 1, strlen(argv[1]), fin);
-    for(i=1;(nread = fread(txt2, 1, strlen(argv[1]), fin)) > 0;++i)
-    // for (i = 1; ((fgets(txt2, strlen(argv[1]) + 1, fin)) != NULL); ++i)
+    while ((nread = fread(txt2, 1, strlen(argv[1]), fin)) > 0)
     {
         strcpy(txt1, txt3);
-        // fgets(txt2, strlen(argv[1])+1, fin);
         strcat(txt1, txt2);
         index = 0;
         while ((pos = strstr(txt1 + index, argv[1])) != NULL)
@@ -54,24 +58,14 @@ int main(int argc, char *argv[])
             if ((indinv - strlen(argv[1]) != indin) && (indsfv - strlen(argv[1]) != indsf))
                 count++;
         }
-        // s = false;
-        // index = 0;
-        // while ((pos = strstr(txt2 + index, argv[1])) != NULL)
-        // {
-        //     index = (pos - txt2) + 1;
-        //     s = true;
-        //     break;
-        // }
-        // if (s != true)
         indinv = indin;
         indsfv = indsf;
-
         strcpy(txt3, txt2);
-        // else
-        //     memset(txt3, 0, strlen(txt3));
     }
-
-    printf("%d times in file\n", count);
-
+    printf("De %d ori in fisier\n", count);
+    free(txt1);
+    free(txt2);
+    free(txt3);
+    fclose(fin);
     return 0;
 }
